f87b675a1afeb4ecc80c22fb14db1e3e291dbb8e
Ephraim Kigamba <ekigamba@ona.io>
2018-05-03 19:00:54 +0300

    Added Credentials Validator test

---
+++ app/src/test/java/com/kigamba/mvp/validators/CredentialsValidatorTest.java
@@ -0,0 +1,33 @@
+package com.kigamba.mvp.validators;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ephraim Kigamba - ekigamba@ona.io on 03/05/2018.
+ */
+public class CredentialsValidatorTest {
+
+    @Test
+    public void isCredentialsOkShouldReturnUsernameError() throws Exception {
+        int actualValue = CredentialsValidator.isCredentialsOk("", "");
+
+        assertEquals(CredentialsValidator.USERNAME_ERROR, actualValue);
+    }
+
+    @Test
+    public void isCredentialsOkShouldReturnPasswordError() throws Exception {
+        int actualValue = CredentialsValidator.isCredentialsOk("username", "");
+
+        assertEquals(CredentialsValidator.PASSWORD_ERROR, actualValue);
+    }
+
+    @Test
+    public void isCredentialsOkShouldReturnCredentialsOk() throws Exception {
+        int actualValue = CredentialsValidator.isCredentialsOk("username", "password");
+
+        assertEquals(CredentialsValidator.CREDENTIALS_OK, actualValue);
+    }
+
+}
